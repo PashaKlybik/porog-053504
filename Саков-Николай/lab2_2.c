49bc98683706dec85d811dd6115845de88970b8e
sakovnv <sakovnv@gmail.com>
2021-04-26 22:43:06 +0300

    recursion

--- Саков-Николай/lab2_2.c
+++ Саков-Николай/lab2_2.c
@@ -12,6 +12,18 @@ double fact(double num)
 	return n;
 }
 
+double recursion(double x, double value, double precision, double *n)
+{
+	double cur = (pow(-1, (*n) - 1) * (pow(x, 2 * (*n) - 1) / fact(2 * (*n) - 1)));
+	value += cur;
+	(*n)++;
+	if (fabs(cur) < precision)
+	{
+		return value;
+	}
+	return recursion(x, value, precision, n);
+}
+
 int main()
 {	
 	setlocale(0, "rus");
@@ -20,14 +32,21 @@ int main()
 	scanf_s("%lf", &eps);
 	double result = 0;
 	double x, exp = 0;
-	printf("Введите x: ");
-	scanf_s("%lf", &x);
-	double n = 1; 
 	do
 	{
-		exp = (pow(-1, n - 1) * (pow(x, 2 * n - 1) / fact(2 * n - 1)));
-		result += exp;
-		n++;
-	} while (fabs(exp) > eps);
-	printf("sin(x) = %lf  %lf при n = %lf", sin(x), result, n);
+		printf("\nВведите x: ");
+		scanf_s("%lf", &x);
+		double n = 1; 
+		do
+		{
+			exp = (pow(-1, n - 1) * (pow(x, 2 * n - 1) / fact(2 * n - 1)));
+			result += exp;
+			n++;
+		} while (fabs(exp) > eps);
+		exp = 0;
+		printf("sin(x) = %.10lf  %.10lf при n = %.0lf\n", sin(x), result, n - 1);
+		n = 1;
+		result = recursion(x, exp, eps, &n);
+		printf("Рекурсивный метод %.10lf при n = %.0lf", result, n - 1);
+	} while (1);
 }
