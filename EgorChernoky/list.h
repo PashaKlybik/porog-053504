f61264cd302bf7a97ebab3214de6499713c26e30
Egor Chernooky <e.chernooky@gmail.com>
2021-06-12 00:24:20 +0300

    Add lab7

---
+++ EgorChernoky/list.h
@@ -0,0 +1,25 @@
+#ifndef EGORCHERNOKY_LIST_H
+#define EGORCHERNOKY_LIST_H
+
+#include <stdlib.h>
+
+struct node{
+    int data;
+    struct node *next, *prev;
+};
+struct node *node(int data, struct node *next, struct node *prev);
+
+struct list{
+    struct node *head;
+    struct node *tail;
+    // list is looped, we don't need tail because it is always before head :)
+    int size;
+};
+struct list *list();
+void pushBack(struct list *l, int value);
+void popBack(struct list *l);
+void pushFront(struct list *l, int value);
+void popFront(struct list *l);
+void deleteList(struct list *l);
+
+#endif //EGORCHERNOKY_LIST_H
