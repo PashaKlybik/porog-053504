8e71d462902e0b6ff378b5123f19a5b79fdefe3d
MariaShchegl <84998169+MariaShchegl@users.noreply.github.com>
2021-06-12 16:48:54 +0300

    gh

---
+++ MariaShchegl/ПРОГР_4_1.cpp
@@ -0,0 +1,50 @@
+﻿#include <stdio.h>
+#include <conio.h>
+#include <locale.h>
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    char myString[100]; 
+    char c;
+    printf("Введите длинную строку: ");
+    int i = 0;
+    int brackets = 0;//скобки
+    while ((c = getchar()) != '\n')
+    {
+        if (c == '(')
+        {
+            brackets++;
+            myString[i] = c;
+            i++;
+            continue;
+        }
+        if (c == ')')
+        {
+            brackets--;
+            myString[i] = c;
+            i++;
+            continue;
+
+        }
+        if (brackets > 0)
+        {
+            continue;
+        }
+        myString[i] = c;
+        i++;
+    }
+   myString[i] = '\0';
+    printf("%s\n", myString);
+    if (brackets > 0)
+    {
+        printf("У вас не закрыто %d скобок", brackets);
+
+    }
+    else if (brackets < 0)
+    {
+        printf("У вас не открыто %d скобок", brackets * -1);
+    }
+
+    }
+   
+    
