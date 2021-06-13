ce0ef2f5633a876827ccf80d5025ea609b6faf57
Egorhypx <dudoser.87@mail.ru>
2021-06-14 00:54:39 +0300

    lab5

--- EgorGolubitskiy/5.1(27).c
+++ EgorGolubitskiy/5.1(27).c
@@ -72,31 +72,25 @@ struct Queue* createQueue()
 }
 void enQueue(struct Queue* q, char k)
 {
-    // Create a new LL node
     struct QNode* temp = newNode(k);
 
-    // If queue is empty, then new node is front and rear both
     if (q->rear == NULL) {
         q->front = q->rear = temp;
         return;
     }
 
-    // Add the new node at the end of queue and change rear
     q->rear->next = temp;
     q->rear = temp;
 }
 void deQueue(struct Queue* q)
 {
-    // If queue is empty, return NULL.
     if (q->front == NULL)
         return;
 
-    // Store previous front and move front one node ahead
     struct QNode* temp = q->front;
 
     q->front = q->front->next;
 
-    // If front becomes NULL, then change rear also as NULL
     if (q->front == NULL)
         q->rear = NULL;
 
