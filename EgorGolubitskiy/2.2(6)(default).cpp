a50e089ba0d9bd37d87360ae56ef268631aa44e7
Egorhypx <dudoser.87@mail.ru>
2021-06-13 21:04:35 +0300

    lab2

---
+++ EgorGolubitskiy/2.2(6)(default).cpp
@@ -0,0 +1,42 @@
+﻿#include <stdio.h>
+#include<math.h>
+
+int Solution(double x, double eps);
+long double Finding_fact(int n);
+
+int main()
+{
+	float x, eps;
+
+	printf("Enter eps: ");
+	scanf_s("%f", &eps);
+	printf("Enter x: ");
+	scanf_s("%f", &x);
+
+	printf("%d", Solution(x, eps));
+
+}
+
+long double Finding_fact(int n) {
+	if (n < 0)
+		return 0;
+	else if (n == 0)
+		return 1;
+	else
+		return n * Finding_fact(n - 1);
+}
+
+int Solution(double x, double eps) {
+
+	long double pr = 0;
+
+	int k, f, n = 0;
+
+	for (int i = 1; fabs(sin(x) - pr) > eps; i++) {
+		k = i - 1;
+		f = 2 * i - 1;
+		pr += pow(-1, k) * pow(x, f) / Finding_fact(2 * i - 1);
+		n = i;
+	}
+	return n;
+}
---
+++ EgorGolubitskiy/2.2(6)(recur).cpp
@@ -0,0 +1,42 @@
+﻿#include <stdio.h>
+#include<math.h>
+
+long double Finding_fact(int n);
+float Solution(float x, float eps, int n, float diff);
+
+int main()
+{
+	float x, eps, diff = 0;
+	int n = 1;
+	float k = n - 1;
+	float f = 2 * n - 1;
+
+	printf("Enter eps: ");
+	scanf_s("%f", &eps);
+	printf("Enter x: ");
+	scanf_s("%f", &x);
+
+	printf("n= %f", Solution(x, eps, n, diff));
+
+}
+
+long double Finding_fact(int n) {
+	if (n < 0)
+		return 0;
+	else if (n == 0)
+		return 1;
+	else
+		return n * Finding_fact(n - 1);
+}
+
+float Solution(float x, float eps, int n, float diff) {
+	float k = n - 1;
+	float f = 2 * n - 1;
+
+	diff += pow(-1, k) * pow(x, f) / Finding_fact(2 * n - 1);
+	if (fabs(sin(x) - diff) < eps)
+		return n;
+	else
+		return Solution(x, eps, n + 1, diff);
+
+}
