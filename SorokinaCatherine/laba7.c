538c022fd30eac220c08bbfbbcb9e874af4595ae
kate-sorokinaa <kate_sorokinaa@mail.ru>
2021-06-14 08:31:36 +0300

    Changed

--- SorokinaCatherine/laba7.c
+++ SorokinaCatherine/laba7.c
@@ -22,6 +22,9 @@ typedef struct node{
 typedef struct{
     Node* head;
     int size;
+    int budgetPlaces;
+    char* specialtyName;
+    char* shortSpecialtyName;
 }List;
 
 void pushBack(List* l, Applicant* ap);
@@ -31,34 +34,125 @@ void clearList(List* l);
 void setInfo(Applicant* ap);
 void getInfo(Applicant* ap);
 int menu();
-void addApplicant(char* faculty,char* specialty, List* FKSiS[4],List* FITY[5],List* IEF[4], Applicant* p);
+void addApplicant(Applicant* p, char* faculty, char* specialty, List* FKSiS[4],List* FITY[5],List* IEF[4]);
 void deleteApplicant(char* name, List* FKSiS[4],List* FITY[5],List* IEF[4]);
-void entryPoints(List* spec, int budg);
+int entryPoints(List* spec);
+void showEntryPoints(List* faculty[], int size);
+void showEnrolled(List* faculty[], int size);
+char* getString();
+void writeToFile(List* faculty[], int size, char* fac, FILE* f);
+void writeFromFile(List* FKSiS[4],List* FITY[5],List* IEF[4], FILE* f);
+void initialization(List* FKSiS[4],List* FITY[5],List* IEF[4]);
 
 int main() {
 
-    int m = 1;
+    int m;
     List* FKSiS[4];
     List* FITY[5];
     List* IEF[4];
 
-    for(int i=0; i<4; i++){
-        FKSiS[i]=(List*) malloc(sizeof(List));
-        FKSiS[i]->size=0;
-    }
-    for(int i=0; i<5; i++){
-        FITY[i]=(List*) malloc(sizeof(List));
-        FITY[i]->size=0;
-    }
-    for(int i=0; i<4; i++){
-        IEF[i]=(List*) malloc(sizeof(List));
-        IEF[i]->size=0;
-    }
+    initialization(FKSiS, FITY, IEF);
 
-    char str[200];
     FILE *f;
     f = fopen("/Users/katasorokina/my projects/sea7/applicants.txt","r");
+    writeFromFile(FKSiS, FITY, IEF, f);
+    fclose(f);
 
+    m = menu();
+    while(m!=5) {
+        switch (m) {
+            case 1: {
+                char* faculty;
+                char* specialty;
+                Applicant *p = (Applicant*) malloc(sizeof(Applicant));
+                setInfo(p);
+                fflush(stdin);
+                printf("Факультет: ");
+                faculty = getString();
+                printf("Специальность: ");
+                specialty = getString();
+                m = menu();
+                addApplicant(p, faculty, specialty, FKSiS, FITY, IEF);
+                break;
+            }
+            case 2: {
+                fflush(stdin);
+                printf("Введите ФИО абитуриента");
+                char* name = getString();
+                deleteApplicant(name, FKSiS, FITY, IEF);
+                m = menu();
+                break;
+            }
+            case 3: {
+                fflush(stdin);
+                printf("\nФакультет компьютерных систем и сетей:");
+                showEntryPoints(FKSiS, 4);
+                printf("\n\nФакультет информационных технологий и управления:");
+                showEntryPoints(FITY, 5);
+                printf("\n\nИнженерно-экономический факультет:");
+                showEntryPoints(IEF, 4);
+                m = menu();
+                break;
+            }
+            case 4: {
+                fflush(stdin);
+                printf("\nСписки поступивших: ");
+                printf("\nФакультет компьютерных систем и сетей:");
+                showEnrolled(FKSiS, 4);
+                printf("\nФакультет информационных технологий и управления:");
+                showEnrolled(FITY, 5);
+                printf("\nИнженерно-экономический факультет:");
+                showEnrolled(IEF, 4);
+                m = menu();
+                break;
+            }
+            default: break;
+        }
+    }
+
+    f = fopen("/Users/katasorokina/my projects/sea7/applicants.txt","w");
+    writeToFile(FKSiS, 4, "ФКСиС", f);
+    writeToFile(FITY, 5, "ФИТУ", f);
+    writeToFile(IEF, 4, "ИЭФ", f);
+    fclose(f);
+
+    for(int i=0; i<4; i++){
+        clearList(FKSiS[i]);
+        free(FKSiS[i]);
+    }
+    for(int i=0; i<5; i++){
+        clearList(FITY[i]);
+        free(FITY[i]);
+    }
+    for(int i=0; i<4; i++){
+        clearList(IEF[i]);
+        free(IEF[i]);
+    }
+
+    return 0;
+}
+
+void writeToFile(List* faculty[], int size, char* fac, FILE* f){
+    for(int i = 0; i < size; i++){
+        Node *tmp = faculty[i]->head;
+        for (int j = 0; j < faculty[i]->size; j++) {
+            fprintf(f,"%s|",fac);
+            fprintf(f,"%s|",faculty[i]->shortSpecialtyName);
+            fprintf(f,"%s|",tmp->data->fullName);
+            fprintf(f,"%s|",tmp->data->passportSerialNumber);
+            fprintf(f,"%s|",tmp->data->address);
+            fprintf(f,"%s|",tmp->data->school);
+            fprintf(f,"%d|",tmp->data->certificate);
+            fprintf(f,"%d|",tmp->data->language);
+            fprintf(f,"%d|",tmp->data->maths);
+            fprintf(f,"%d\n",tmp->data->physics);
+            tmp=tmp->next;
+        }
+    }
+}
+
+void writeFromFile(List* FKSiS[4],List* FITY[5],List* IEF[4], FILE* f){
+    char str[200];
     while(1){
         char *string = fgets(str, sizeof(str),f);
         Applicant *p = (Applicant*) malloc(sizeof(Applicant));
@@ -66,120 +160,97 @@ int main() {
         if (string==NULL)
             break;
 
-        char* faculty = strtok(string, "|");
-        char* specialty = strtok(NULL, "|");
+        char* faculty = strtok(string, "|\n");
+        char* specialty = strtok(NULL, "|\n");
 
-        p->fullName = strtok(NULL, "|");
-        p->passportSerialNumber = strtok(NULL, "|");
-        p->address = strtok(NULL, "|");
-        p->school = strtok(NULL, "|");
-        char* num = strtok(NULL, "|");
+        p->fullName = strtok(NULL, "|\n");
+        p->passportSerialNumber = strtok(NULL, "|\n");
+        p->address = strtok(NULL, "|\n");
+        p->school = strtok(NULL, "|\n");
+        char* num = strtok(NULL, "|\n");
         p->certificate = atol(num);
-        num = strtok(NULL, "|");
+        num = strtok(NULL, "|\n");
         p->language = atol(num);
-        num = strtok(NULL, "|");
+        num = strtok(NULL, "|\n");
         p->maths = atol(num);
-        num = strtok(NULL, "|");
+        num = strtok(NULL, "|\n");
         p->physics = atol(num);
 
-        addApplicant(faculty, specialty, FKSiS, FITY, IEF, p);
+        addApplicant(p, faculty, specialty, FKSiS, FITY, IEF);
+    }
+}
 
+void showEntryPoints(List* faculty[], int size){
+    for (int i = 0; i < size; i++) {
+        printf("\n%s: %d", faculty[i]->specialtyName, entryPoints(faculty[i]));
+    }
+}
+
+int entryPoints(List* spec){
+    if(spec->size==0)
+        return 0;
+    int res;
+    int *ball = (int*) malloc((spec->size)*sizeof(int));
+    Node* tmp=spec->head;
+    for (int i = 0; i < spec->size; i++) {
+        ball[i]=tmp->data->physics+tmp->data->language+tmp->data->certificate+tmp->data->maths;
+        tmp=tmp->next;
     }
 
-    fclose(f);
-    while(m!=5) {
-        m = menu();
-        switch (m) {
-            case 1: {
-                Applicant *p = (Applicant*) malloc(sizeof(Applicant));
-                setInfo(p);
+    for(int i = 0 ; i < spec->size - 1; i++) {
+        for(int j = 0 ; j < spec->size - i - 1 ; j++) {
+            if(ball[j] < ball[j+1]) {
+                int temp = ball[j];
+                ball[j] = ball[j+1] ;
+                ball[j+1] = temp;
             }
-            case 2: {
-                printf("Введите ФИО абитуриента");
-                char* name = (char*) malloc(35*sizeof(char));
-                scanf("%[^\n]s", name);
-                deleteApplicant(name, FKSiS, FITY, IEF);
-            }
-            case 3: {
-                printf("\nФКСиС:");
-                printf("\nИиТП:");
-                printf("\nПОИТ:");
-                printf("\nВМСиС:");
-                printf("\nЭВС:");
-
-                printf("\nФИТУ:");
-                printf("\nАСОИ:");
-                printf("\nИИ:");
-                printf("\nИТиУТС:");
-                printf("\nПЭ:");
-                printf("\nИСиТ:");
-
-                printf("\nИЭФ:");
-                printf("\nИСиТ(э):");
-                printf("\nИСиТ(л):");
-                printf("\nЭЭБ:");
-                printf("\nЭМ:");
-            }
-            case 4: {
-
-            }
-            default: break;
         }
     }
 
-    return 0;
+    if(spec->budgetPlaces<spec->size){
+        res = ball[spec->budgetPlaces-1];
+    } else{
+        res = ball[spec->size-1];
+    }
+
+    free(ball);
+    return res;
 }
 
-void entryPoints(List* spec, int budg){
-
-
+void showEnrolled(List* faculty[], int size){
+    for (int i = 0; i < size; i++) {
+        Node *tmp = faculty[i]->head;
+        for (int j = 0; j < faculty[i]->size; j++) {
+            int score = tmp->data->physics + tmp->data->language + tmp->data->certificate + tmp->data->maths;
+            if (score >= entryPoints(faculty[i])) {
+                getInfo(tmp->data);
+            }
+            tmp = tmp->next;
+        }
+    }
 }
 
-void addApplicant(char* faculty,char* specialty, List* FKSiS[4],List* FITY[5],List* IEF[4], Applicant* p){
+void addApplicant(Applicant* p, char* faculty, char* specialty, List* FKSiS[4],List* FITY[5],List* IEF[4]){
     if(strcmp(faculty,"ФКСиС")==0){
-        if(strcmp(specialty,"ИиТП")==0) {
-            FKSiS[0]=(List*) realloc(FKSiS[0],(FKSiS[0]->size)* sizeof(List));
-            pushBack(FKSiS[0], p);
-        } else if(strcmp(specialty,"ПОИТ")==0) {
-            FKSiS[1]=(List*) realloc(FKSiS[1],(FKSiS[1]->size)* sizeof(List));
-            pushBack(FKSiS[1], p);
-        } else if(strcmp(specialty,"ВМСиС")==0) {
-            FKSiS[2]=(List*) realloc(FKSiS[2],(FKSiS[2]->size)* sizeof(List));
-            pushBack(FKSiS[2], p);
-        } else if(strcmp(specialty,"ЭВС")==0) {
-            FKSiS[3]=(List*) realloc(FKSiS[3],(FKSiS[3]->size)* sizeof(List));
-            pushBack(FKSiS[3], p);
+        for (int i = 0; i < 4; i++) {
+            if(strcmp(specialty, FKSiS[i]->shortSpecialtyName)==0) {
+                pushBack(FKSiS[i], p);
+                return;
+            }
         }
     } else if(strcmp(faculty,"ИЭФ")==0){
-        if(strcmp(specialty,"ИСиТ(э)")==0) {
-            IEF[0]=(List*) realloc(IEF[0],(IEF[0]->size)* sizeof(List));
-            pushBack(IEF[0], p);
-        } else if(strcmp(specialty,"ИСиТ(л)")==0) {
-            IEF[1]=(List*) realloc(IEF[1],(IEF[1]->size)* sizeof(List));
-            pushBack(IEF[1], p);
-        } else if(strcmp(specialty,"ЭЭБ")==0) {
-            IEF[2]=(List*) realloc(IEF[2],(IEF[2]->size)* sizeof(List));
-            pushBack(IEF[2], p);
-        } else if(strcmp(specialty,"ЭМ")==0) {
-            IEF[3]=(List*) realloc(IEF[3],(IEF[3]->size)* sizeof(List));
-            pushBack(IEF[3], p);
+        for (int i = 0; i < 4; i++) {
+            if(strcmp(specialty, IEF[i]->shortSpecialtyName)==0) {
+                pushBack(IEF[i], p);
+                return;
+            }
         }
     }else if(strcmp(faculty,"ФИТУ")==0){
-        if(strcmp(specialty,"АСОИ")==0) {
-            FITY[0]=(List*) realloc(FITY[0],(FITY[0]->size)* sizeof(List));
-            pushBack(FITY[0], p);
-        } else if(strcmp(specialty,"ИИ")==0) {
-            FITY[1]=(List*) realloc(FITY[1],(FITY[1]->size)* sizeof(List));
-            pushBack(FITY[1], p);
-        } else if(strcmp(specialty,"ИТиУТС")==0) {
-            FITY[2]=(List*) realloc(FITY[2],(FITY[2]->size)* sizeof(List));
-            pushBack(FITY[2], p);
-        } else if(strcmp(specialty,"ПЭ")==0) {
-            FITY[3]=(List*) realloc(FITY[3],(FITY[3]->size)* sizeof(List));
-            pushBack(FITY[3], p);
-        } else if(strcmp(specialty,"ИСиТ")==0) {
-            FITY[4]=(List*) realloc(FITY[4],(FITY[3]->size)* sizeof(List));
-            pushBack(FITY[4], p);
+        for (int i = 0; i < 5; i++) {
+            if(strcmp(specialty, FITY[i]->shortSpecialtyName)==0) {
+                pushBack(FITY[i], p);
+                return;
+            }
         }
     }
 }
@@ -219,8 +290,8 @@ int menu(){
            "\n2 - забрать документы"
            "\n3 - узнать проходные баллы"
            "\n4 - увидеть списки поступивших"
-           "\n5 - выйти");
-    while(var!=1 && ch<6 && ch>0) {
+           "\n5 - выйти\n");
+    while(var!=1 && (ch>5 || ch<1)) {
         var = scanf("%d", &ch);
         if(var!=1)
             fflush(stdin);
@@ -228,44 +299,156 @@ int menu(){
     return ch;
 }
 
+void initialization(List* FKSiS[4],List* FITY[5],List* IEF[4]){
+
+    for(int i=0; i<4; i++){
+        FKSiS[i]=(List*) malloc(sizeof(List));
+        FKSiS[i]->size=0;
+        FKSiS[i]->head=NULL;
+    }
+    for(int i=0; i<5; i++){
+        FITY[i]=(List*) malloc(sizeof(List));
+        FITY[i]->size=0;
+        FITY[i]->head=NULL;
+    }
+    for(int i=0; i<4; i++){
+        IEF[i]=(List*) malloc(sizeof(List));
+        IEF[i]->size=0;
+        IEF[i]->head=NULL;
+    }
+
+    FKSiS[0]->budgetPlaces=65;
+    FKSiS[1]->budgetPlaces=60;
+    FKSiS[2]->budgetPlaces=75;
+    FKSiS[3]->budgetPlaces=25;
+
+    FITY[0]->budgetPlaces=60;
+    FITY[1]->budgetPlaces=55;
+    FITY[2]->budgetPlaces=60;
+    FITY[3]->budgetPlaces=25;
+    FITY[4]->budgetPlaces=15;
+
+    IEF[0]->budgetPlaces=60;
+    IEF[1]->budgetPlaces=20;
+    IEF[2]->budgetPlaces=15;
+    IEF[3]->budgetPlaces=15;
+
+    FKSiS[0]->specialtyName="Информатика и технологии программирования";
+    FKSiS[1]->specialtyName="Программное обеспечение информационных технологий";
+    FKSiS[2]->specialtyName="Вычислительные машины, системы и сети";
+    FKSiS[3]->specialtyName="Электронные вычислительные средства";
+
+    FITY[0]->specialtyName="Автоматизированные системы обработки информации";
+    FITY[1]->specialtyName="Искусственный интеллект";
+    FITY[2]->specialtyName="Информационные технологии и управление в технических системах";
+    FITY[3]->specialtyName="Промышленная электроника";
+    FITY[4]->specialtyName="Информационные системы и технологии (в игровой индустрии)";
+
+    IEF[0]->specialtyName="Информационные системы и технологии (в экономике)";
+    IEF[1]->specialtyName="Информационные системы и технологии (в логистике)";
+    IEF[2]->specialtyName="Экономика электронного бизнеса";
+    IEF[3]->specialtyName="Электронный маркетинг";
+
+    FKSiS[0]->shortSpecialtyName="ИиТП";
+    FKSiS[1]->shortSpecialtyName="ПОИТ";
+    FKSiS[2]->shortSpecialtyName="ВМСиС";
+    FKSiS[3]->shortSpecialtyName="ЭВС";
+
+    FITY[0]->shortSpecialtyName="АСОИ";
+    FITY[1]->shortSpecialtyName="ИИ";
+    FITY[2]->shortSpecialtyName="ИТиУТС";
+    FITY[3]->shortSpecialtyName="ПЭ";
+    FITY[4]->shortSpecialtyName="ИСиТ";
+
+    IEF[0]->shortSpecialtyName="ИСиТ(э)";
+    IEF[1]->shortSpecialtyName="ИСиТ(л)";
+    IEF[2]->shortSpecialtyName="ЭЭБ";
+    IEF[3]->shortSpecialtyName="ЭМ";
+}
+
 void getInfo(Applicant* ap){
-    printf("ФИО: %s\tБалл: %d", ap->fullName, ap->certificate+ap->maths+ap->language+ap->physics);
+    printf("\nФИО: %s\t\t\tБалл: %d", ap->fullName, ap->certificate+ap->maths+ap->language+ap->physics);
 }
 
 void setInfo(Applicant* ap){
+    fflush(stdin);
     printf("\nВведите информацию об абитуриенте:");
     printf("\nФИО: ");
-    scanf("%[^\n]s", ap->fullName);
-    printf("\nСерийный номер паспорта: ");
-    scanf("%[^\n]s", ap->passportSerialNumber);
-    printf("\nАдрес: ");
-    scanf("%[^\n]s", ap->address);
-    printf("\nУчебное заведение: ");
-    scanf("%[^\n]s", ap->school);
-    printf("\nБалл из аттестата: ");
-    scanf("%d",&ap->certificate);
-    printf("\nБалл за ЦТ по языку: ");
-    scanf("%d",&ap->language);
-    printf("\nБалл за ЦТ по математике: ");
-    scanf("%d",&ap->maths);
-    printf("\nБалл за ЦТ по физике: ");
-    scanf("%d",&ap->physics);
+    ap->fullName=getString();
+    printf("Серийный номер паспорта: ");
+    ap->passportSerialNumber=getString();
+    printf("Адрес: ");
+    ap->address=getString();
+    printf("Учебное заведение: ");
+    ap->school=getString();
+    printf("Балл из аттестата: ");
+    scanf("%d",&(ap->certificate));
+    printf("Балл за ЦТ по языку: ");
+    scanf("%d",&(ap->language));
+    printf("Балл за ЦТ по математике: ");
+    scanf("%d",&(ap->maths));
+    printf("Балл за ЦТ по физике: ");
+    scanf("%d",&(ap->physics));
+}
+
+char* getString(){
+    char* string=(char*)malloc(60* sizeof(char));
+    char symbol = 'a';
+    int index = 0;
+    while(symbol!='\n'){
+        symbol= getc(stdin);
+        if(symbol!='\n') {
+            string[index] = symbol;
+            index++;
+        }
+    }
+    string[index]='\0';
+    return string;
 }
 
 void pushBack(List* l, Applicant* ap){
-    if(l->head==NULL){
+    Node *tmp = l->head;
+    if(tmp==NULL){
         l->head=(Node*)malloc(sizeof(Node));
         l->head->next=NULL;
         l->head->prev=NULL;
-        l->head->data=ap;
+        l->head->data=(Applicant*) malloc(sizeof(Applicant));
+        l->head->data->fullName=(char*)malloc(60*sizeof(char));
+        strcpy(l->head->data->fullName, ap->fullName);
+        strcpy(ap->fullName, "");
+        l->head->data->passportSerialNumber=(char*)malloc(10*sizeof(char));
+        strcpy(l->head->data->passportSerialNumber, ap->passportSerialNumber);
+        strcpy(ap->passportSerialNumber, "");
+        l->head->data->address=(char*)malloc(60*sizeof(char));
+        strcpy(l->head->data->address, ap->address);
+        strcpy(ap->address, "");
+        l->head->data->school=(char*)malloc(60*sizeof(char));
+        strcpy(l->head->data->school, ap->school);
+        strcpy(ap->school, "");
+        l->head->data->certificate=ap->certificate;
+        l->head->data->language=ap->language;
+        l->head->data->maths=ap->maths;
+        l->head->data->physics=ap->physics;
     } else{
-        Node *tmp = l->head;
         while (tmp->next!=NULL){
             tmp=tmp->next;
         }
+        tmp->next=(Node*)malloc(sizeof(Node));
         (tmp->next)->next=NULL;
-        (tmp->next)->data=ap;
         (tmp->next)->prev=tmp;
+        (tmp->next)->data=(Applicant*) malloc(sizeof(Applicant));
+        (tmp->next)->data->fullName=(char*)malloc(60*sizeof(char));
+        strcpy((tmp->next)->data->fullName, ap->fullName);
+        (tmp->next)->data->passportSerialNumber=(char*)malloc(10*sizeof(char));
+        strcpy((tmp->next)->data->passportSerialNumber, ap->passportSerialNumber);
+        (tmp->next)->data->address=(char*)malloc(60*sizeof(char));
+        strcpy((tmp->next)->data->address, ap->address);
+        (tmp->next)->data->school=(char*)malloc(60*sizeof(char));
+        strcpy((tmp->next)->data->school, ap->school);
+        (tmp->next)->data->certificate=ap->certificate;
+        (tmp->next)->data->language=ap->language;
+        (tmp->next)->data->maths=ap->maths;
+        (tmp->next)->data->physics=ap->physics;
     }
     l->size++;
 }
@@ -290,11 +473,9 @@ void removeThis(List* l, int index){
             tmp=tmp->next;
         }
         Node *del = tmp->next;
-        if(tmp->next!=NULL){
-            tmp->next = del->next;
-            if(del->next!=NULL){
-                (del->next)->prev=tmp;
-            }
+        tmp->next = del->next;
+        if(del->next!=NULL) {
+            (del->next)->prev = tmp;
         }
         free(del->data);
         free(del);
