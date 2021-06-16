37dbf61a3cea756eeb92ec3ba1878e5daf96275f
GeorgyVoronich <79355268+GeorgyVoronich@users.noreply.github.com>
2021-06-16 03:35:14 +0300

    Воронич Георгий lab1 var. #30 (#205)
    * First commit
    
    * Update lab1.c

---
+++ GeorgyVoronich/lab1.c
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string.h>
+#include <conio.h>
+#include <windows.h>
+#include <locale.h>
+
+
+int main()
+{
+	int k = 10;
+	int n = (k - 1) / 20 + 1;
+	int ch = ((k - 1) % 20) / 2;
+	int s =(k % 2)* n + ((k + 1) % 2) * ch;
+	printf("%d", s);
+	return 0;
+}
