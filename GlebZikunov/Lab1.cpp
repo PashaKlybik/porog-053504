0bc38d66800361ec39a78356fd820d6c4e6f1e31
Gleb <79657936+GlebZikunov@users.noreply.github.com>
2021-06-16 03:52:27 +0300

    FirstLab (#139)

---
+++ GlebZikunov/Lab1.cpp
@@ -0,0 +1,30 @@
+﻿#include <stdio.h> 
+
+int main()
+{
+    int i;
+    printf("\n\n\n\t\t\t\t");
+
+    for (i = 1; i <= 1000; i++)
+    {
+        if ( i == (i * i) % 10 && i < 9)
+        {
+            printf("%d\t", i);
+        }
+
+        else if (i == (i * i) % 100 && i > 9 && i < 99)
+        {
+            printf("%d\t", i);
+        }
+
+        else if (i == (i * i) % 1000 && i > 99)
+        {
+            printf("%d\t", i);
+        }
+
+    }
+
+    printf("\n\n\n");
+
+    return 0;
+}
